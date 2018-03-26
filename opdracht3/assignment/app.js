62b2d7bc88d9ab0c58e1dd78147cba8ae02758f8
VincentKempers <vincent.m.kempers@gmail.com>
2018-03-26 14:38:50 +0200

    Added detail page and rendered the index page with a tags

--- opdracht3/assignment/app.js
+++ opdracht3/assignment/app.js
@@ -7,7 +7,6 @@ app.set('view engine', 'pug');
 app.use('/', function(req, res){
   request('https://uinames.com/api/?amount=25', function (error, response, body) {
     var data = JSON.parse(body);
-    console.log(data)
     var contacts = data.map(function(d){
       return {
         name: d.name + ' ' + d.surname,
@@ -15,6 +14,23 @@ app.use('/', function(req, res){
       }
     })
     res.render('index', {data: contacts});
+  });
+});
+
+
+app.use('/:id', function(req, res){
+  request('https://uinames.com/api/?ext', function (error, response, body) {
+    var data = JSON.parse(body);
+    var contact = data.map(function(d){
+      return {
+        name: d.title + ' ' + d.name + ' ' + d.surname,
+        region: d.region,
+        birthday: d.birthday.dmy,
+        email: d.email,
+        image: d.photo
+      }
+    })
+    res.render('detail', {data: contact});
   });
 });
 
