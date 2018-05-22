48698675e43a6ca307dccb906ceb36108b93d852
VincentKempers <me@vncn.tk>
2018-05-22 15:13:24 +0200

    add EJS and refactored indexjs

--- opdracht3/assignment/app.js
+++ opdracht3/assignment/app.js
@@ -2,15 +2,27 @@ var express = require('express');
 var faker = require('faker');
 var app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
+app.use('/static', express.static('static'));
+
+// storing all the users in an array
 var allUsers = [];
 
+// function to sort the data.
+function compare(a,b) {
+  if (a.name < b.name)
+    return -1;
+  if (a.name > b.name)
+    return 1;
+  return 0;
+}
+// looping over all data to push in fake user!
 for (var i = 0; i < 15; i++) {
-  var fakeUser = {
-    card: faker.helpers.createCard()
-  };
-  allUsers.push(fakeUser)
+  var fakeUser = faker.helpers.createCard();
+  // push all data to allUsers
+  allUsers.push(fakeUser);
+  allUsers.sort(compare);
 };
 
 app.get('/', function(req,res){
@@ -24,9 +36,11 @@ app.use('/contacts', function(req, res){
 app.get('/:id', function(req, res) {
   var rightUser = [];
   allUsers.forEach(function (d) {
-    if (d.card.name === req.params.id) {
-      console.log(d.card);
-      rightUser.push(d.card);
+    // console.log(d.name);
+    if (req.params.id == d.name) {
+      console.log(d.name);
+    } else {
+      console.log('vincent');
     }
   });
   res.render('detail', {fakePerson: rightUser});
