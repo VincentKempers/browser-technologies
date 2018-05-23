9bcb82c6db2121974ce935bc0fe42847660f29c7
VincentKempers <me@vncn.tk>
2018-05-23 16:35:22 +0200

    edit the filter

--- opdracht3/assignment/static/filter.js
+++ opdracht3/assignment/static/filter.js
@@ -3,14 +3,13 @@ function myFunction() {
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName("li");
+  li = ul.getElementsByTagName("dd");
   for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
           li[i].style.display = "";
       } else {
           li[i].style.display = "none";
-
       }
   }
 }
