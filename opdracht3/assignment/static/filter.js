d5f96a7332f660f86d0578e4bbe2cff16dc4cb18
VincentKempers <me@vncn.tk>
2018-05-23 17:52:18 +0200

    add filterjs

--- opdracht3/assignment/static/filter.js
+++ opdracht3/assignment/static/filter.js
@@ -1,15 +1,36 @@
-function myFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName("dd");
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
+// This shouldn't work but it does on IE8 !? SUP WITH THAT JAVASCRIPT
+if (document.getElementById && document.getElementsByTagName && document.attachEvent) {
+  var input = document.getElementById("myInput");
+  input.attachEvent('onkeyup', myFunction);
+
+  function myFunction() {
+    var input = document.getElementById("myInput");
+    var filter = input.value.toUpperCase();
+    var ul = document.getElementById("myUL");
+    var dd = ul.getElementsByTagName("dd");
+    for (var i = 0; i < dd.length; i++) {
+      var a = dd[i].getElementsByTagName("a")[0];
       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
+        dd[i].style.display = "";
       } else {
-          li[i].style.display = "none";
+        dd[i].style.display = "none";
       }
+    }
   }
+} else {
+  var theInput = document.querySelector('#myInput');
+  theInput.addEventListener('keyup', function myFunction() {
+    var input = document.getElementById("myInput");
+    var filter = input.value.toUpperCase();
+    var ul = document.getElementById("myUL");
+    var dd = ul.getElementsByTagName("dd");
+    for (var i = 0; i < dd.length; i++) {
+        var a = dd[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            dd[i].style.display = "";
+        } else {
+            dd[i].style.display = "none";
+        }
+    }
+  }, false );
 }
