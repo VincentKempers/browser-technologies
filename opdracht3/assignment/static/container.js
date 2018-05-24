240bf0bf638440a99dcfe0f178c90460b856efeb
VincentKempers <me@vncn.tk>
2018-05-24 14:58:51 +0200

    small fixes

--- opdracht3/assignment/static/container.js
+++ opdracht3/assignment/static/container.js
@@ -3,15 +3,15 @@ if (!document.attachEvent) {
   var imageShow = document.getElementById('content');
 
   for (var i = 0; i < clickAble.length; i++) {
-    clickAble[i].addEventListener('click', showPicture, true);
+    clickAble[i].addEventListener('mouseover', showPicture, true);
   }
 
   function showPicture(event) {
-    var phoneNumbers = Math.floor(Math.random() * 999 + 1) + '-' + Math.floor(Math.random() * 999 + 1) + '-' + Math.floor(Math.random() * 999 + 1);
+    var phoneNumbers = Math.floor(Math.random() * 999 + 100) + '-' + Math.floor(Math.random() * 999 + 100) + '-' + Math.floor(Math.random() * 999 + 100);
     imageShow.innerHTML = `
       <img src="static/imgs/random-user_imageM4.jpg" alt="">
-      <p>Home: ${phoneNumbers}</p>
-      <p>address: 407 Cummings Point Apt. 883</p>
+      <p>${phoneNumbers}</p>
+      <p>407 Cummings Point Apt. 883</p>
       <p><a href="mailto:Emory_Schmitt22@hotmail.com">Emory_Schmitt22@hotmail.com</a></p>
       <p>username${Math.floor(Math.random() * 2000 + 1)}</p>
     `
