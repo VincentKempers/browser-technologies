dd1cbdff76fc718bc6d8866c51ca351938386364
VincentKempers <me@vncn.tk>
2018-05-24 18:41:09 +0200

    ES6 to ES5

--- opdracht3/assignment/static/container.js
+++ opdracht3/assignment/static/container.js
@@ -1,20 +1,14 @@
-if (!document.attachEvent) {
+if (document.addEventListener) {
   var clickAble = document.getElementsByTagName("dd");
   var imageShow = document.getElementById('content');
 
   for (var i = 0; i < clickAble.length; i++) {
-    clickAble[i].addEventListener('mouseover', showPicture, true);
+    clickAble[i].addEventListener('click', showPicture, true);
   }
 
   function showPicture(event) {
     var phoneNumbers = Math.floor(Math.random() * 999 + 100) + '-' + Math.floor(Math.random() * 999 + 100) + '-' + Math.floor(Math.random() * 999 + 100);
-    imageShow.innerHTML = `
-      <img src="static/imgs/random-user_imageM4.jpg" alt="">
-      <p>${phoneNumbers}</p>
-      <p>407 Cummings Point Apt. 883</p>
-      <p><a href="mailto:Emory_Schmitt22@hotmail.com">Emory_Schmitt22@hotmail.com</a></p>
-      <p>username${Math.floor(Math.random() * 2000 + 1)}</p>
-    `
+    imageShow.innerHTML = "<img src='static/imgs/random-user_imageM4.jpg' alt=''> <p>"+ phoneNumbers +"</p> <p>407 Cummings Point Apt. 883</p> <p><a href='mailto:Emory_Schmitt22@hotmail.com'>Emory_Schmitt22@hotmail.com</a></p> <p>"+ Math.floor(Math.random() * 2000 + 1) + " </p>";
     imageShow.classList.remove('hidden');
     imageShow.classList.add('animationleftToRight');
   }
