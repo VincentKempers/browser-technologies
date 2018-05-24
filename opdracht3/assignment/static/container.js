08d05f493675249977a61412149c4a3e4774d1c3
VincentKempers <me@vncn.tk>
2018-05-24 20:35:25 +0200

    added all that readme goodness

--- opdracht3/assignment/static/container.js
+++ opdracht3/assignment/static/container.js
@@ -8,7 +8,7 @@ if (document.addEventListener) {
 
   function showPicture(event) {
     var phoneNumbers = Math.floor(Math.random() * 999 + 100) + '-' + Math.floor(Math.random() * 999 + 100) + '-' + Math.floor(Math.random() * 999 + 100);
-    imageShow.innerHTML = "<img src='static/imgs/random-user_imageM4.jpg' alt=''> <p>"+ phoneNumbers +"</p> <p>407 Cummings Point Apt. 883</p> <p><a href='mailto:Emory_Schmitt22@hotmail.com'>Emory_Schmitt22@hotmail.com</a></p> <p>"+ Math.floor(Math.random() * 2000 + 1) + " </p>";
+    imageShow.innerHTML = "<img src='static/imgs/random-user_imageM4.jpg' alt=''> <p>"+ phoneNumbers +"</p> <p>407 Cummings Point Apt. 883</p> <p><a href='mailto:Emory_Schmitt22@hotmail.com'>Emory_Schmitt22@hotmail.com</a></p> <p>UserName"+ Math.floor(Math.random() * 2000 + 1) + " </p>";
     imageShow.classList.remove('hidden');
     imageShow.classList.add('animationleftToRight');
   }
